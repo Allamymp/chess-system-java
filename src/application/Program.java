2eaae1b71806eccf757675d2d693ba35f3f641b1
allamymp <allamympereira@gmail.com>
2023-03-06 15:41:28 -0300

    First Class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,23 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		
-		System.out.println("Iam alive2");
 
+		Scanner sc = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
+		Position board = new Position(10,20);
+		System.out.println("Iam alive!");
+
+		
+		System.out.println(board);
+
+		sc.close();
 	}
 
 }
